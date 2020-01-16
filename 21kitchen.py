8e7519c239c403fed1de2f03ffe842f0890fb51f
admin <kateb.975@mail.ru>
2020-01-16 13:47:41 +0300

    all done except host and payment

--- 21kitchen.py
+++ 21kitchen.py
@@ -1,12 +1,20 @@
 import telebot
 from random import randint
 import requests
+#from PIL import Image
+#from io import BytesIO
+#import urllib
 
 #kitchen21_bot 1006074917:AAEuQNPot-SYTvBRFsp0DQ0GeUZc1a4QXJI
 
 telebot.apihelper.proxy = {'https': 'socks5h://geek:socks@t.geekclass.ru:7777'}
 bot = telebot.TeleBot('1006074917:AAEuQNPot-SYTvBRFsp0DQ0GeUZc1a4QXJI')
-
+"""
+url='https://avatars.mds.yandex.net/get-zen_doc/196027/pub_5d716e4b92414d00adae74cf_5d7170da3f548700ae6b35f2/scale_1200'
+f = open('out.jpg','wb')
+f.write(urllib.request.urlopen(url).read())
+f.close()
+"""
 flag = 0    # for confirmation
 order = {}  # order of our client
 
@@ -45,6 +53,20 @@ allmenu = {
     '111':'bread',
 }
 
+price = {
+    '101':'100',
+    '102':'100',
+    '103':'100',
+    '104':'200',
+    '105':'200',
+    '106':'200',
+    '107':'300',
+    '108':'300',
+    '109':'300',
+    '110':'50',
+    '111':'50',
+}
+
 short_description = {
     '101':'вода',
     '102':'чай',
@@ -72,20 +94,31 @@ long_description = {
     '110':'Греночки как греночки',
     '111':'Хлебушек свежий',
 }
-
+"""
+@bot.message_handler(commands=['photo'])
+def photo(message):
+    user_id = message.chat.id
+    response = requests.get('https://avatars.mds.yandex.net/get-zen_doc/196027/pub_5d716e4b92414d00adae74cf_5d7170da3f548700ae6b35f2/scale_1200')
+    img = Image.open(BytesIO(response.content))
+    photo = open('out.jpg', 'rb')
+    bot.send_photo(user_id, photo)
+    photo.close()
+    #bot.send_photo(user_id, img)
+"""
 @bot.message_handler(commands=['start', 'help'])
 def help(message):
     user_id = message.chat.id
-    bot.send_message(user_id, "Today we have: \n" +
+    bot.send_message(user_id, "Сегодня в меню: \n" +
                      "/fullmenu \n/main \n/beverages \n/salads \n/snacks \n" +
-                     "Show my order:\n /cart")
+                     "Моя корзина:\n /cart")
 
 @bot.message_handler(commands=['fullmenu'])
 def fullmenu(message):
     user_id = message.chat.id
     items = 'Choose:\n\n'
     for key, value in allmenu.items():
-        items += short_description.get(key, 0) + "\n"
+        items += short_description.get(key, 0) + " - "
+        items += price.get(key) + "$\n"
         items += ' /' + value + "_description" + "\n"
         items += ' /' + value + "_add_to_cart" + "\n"
         items += '\n'
@@ -96,7 +129,8 @@ def beverages(message):
     user_id = message.chat.id
     items = 'Choose:\n\n'
     for key, value in bever.items():
-        items += short_description.get(key, 0) + "\n"
+        items += short_description.get(key, 0) + " - "
+        items += price.get(key) + "$\n"
         items += ' /' + value + "_description" + "\n"
         items += ' /' + value + "_add_to_cart" + "\n"
         items += '\n'
@@ -107,7 +141,8 @@ def salads(message):
     user_id = message.chat.id
     items = 'Choose:\n\n'
     for key, value in salad.items():
-        items += short_description.get(key, 0) + "\n"
+        items += short_description.get(key, 0) + " - "
+        items += price.get(key) + "$\n"
         items += ' /' + value + "_description" + "\n"
         items += ' /' + value + "_add_to_cart" + "\n"
         items += '\n'
@@ -118,7 +153,8 @@ def snacks(message):
     user_id = message.chat.id
     items = 'Choose:\n\n'
     for key, value in snack.items():
-        items += short_description.get(key, 0) + "\n"
+        items += short_description.get(key, 0) + " - "
+        items += price.get(key) + "$\n"
         items += ' /' + value + "_description" + "\n"
         items += ' /' + value + "_add_to_cart" + "\n"
         items += '\n'
@@ -129,7 +165,8 @@ def maining(message):
     user_id = message.chat.id
     items = 'Choose:\n\n'
     for key, value in main.items():
-        items += short_description.get(key, 0) + "\n"
+        items += short_description.get(key, 0) + " - "
+        items += price.get(key) + "$\n"
         items += ' /' + value + "_description" + "\n"
         items += ' /' + value + "_add_to_cart" + "\n"
         items += '\n'
@@ -139,65 +176,75 @@ def maining(message):
 def show_cart(message):
     user_id = message.chat.id
     if user_id not in order:
-        bot.send_message(user_id, "Your cart is empty.\n/help")
+        bot.send_message(user_id, "Ваша корзина пуста.\n/help")
     else:
-        bot.send_message(user_id, "Your cart:" + '\n\n' + order[user_id]
-                     + "\nDelete any item:" + "\n/delete"
-                     + "\nSee more:" + "\n/fullmenu"
-                     + "\nConfirm order:" + "\n/confirm")
+        str = order[user_id]
+        str = str.split('\n')
+        res = 0
+        str2 = ""
+
+        for i in str:
+            str2 += i
+            for key, value in allmenu.items():
+                if value == i:
+                    num = price.get(key) # стоимость одного
+                    str2 = str2 + " - " + num + "\n"
+                    res += int(num)  # общая стоимость
+                    break
+        bot.send_message(user_id,
+                      "Ваша корзина:" + '\n\n' + str2
+                     + f"\nОбщая стоимость: {res}\n"
+                     + "\nУдалить товар: /delete"
+                     + "\nМеню: /fullmenu"
+                     + "\nПодтвердить заказ: /confirm")
+
 
 @bot.message_handler(commands=['delete'])
 def delete_item(message):
     user_id = message.chat.id
     if user_id not in order or order[user_id] == "":
-        bot.send_message(user_id, "Sorry, you can't delete. \nYour cart is empty.\n/help")
+        bot.send_message(user_id, "Ваша корзина пуста.\n/help")
     else:
-        list = "Which item do u want to delete?\n/"
+        list = "Что удалить?\n/"
         for i in order[user_id]:
             if (i == '\n'):
                 list += "_delete" + i + "/"
             else:
                 list += i
-       # list += "_delete"
         bot.send_message(user_id, list)
 
 @bot.message_handler(commands=['confirm'])
 def confirm(message):
     user_id = message.chat.id
     if user_id not in order or order[user_id] == "":
-        bot.send_message(user_id, "Sorry, you can't confirm.\nYour cart is empty.\n/help")
+        bot.send_message(user_id, "Ваша корзина пуста.\n/help")
     else:
         global flag
         flag = 1
-        bot.send_message(user_id, "Confirm order?\n\n" + order[user_id] + "\n/yes_confirm")
+        str = order[user_id].split("\n")
+        str2 = ""
+        res = 0
+
+        for i in str:
+            str2 += i
+            for key, value in allmenu.items():
+                if value == i:
+                    num = price.get(key)  # стоимость одного
+                    str2 = str2 + " - " + num + "\n"
+                    res += int(num)  # общая стоимость
+                    break
+
+        bot.send_message(user_id, f"Подтвердить заказ?\n\n{str2}\nОбщая стоимость: {res}\n/yes_confirm")
 
 @bot.message_handler(commands=['yes_confirm'])
 def yes_confirm(message):
     user_id = message.chat.id
     if(flag == 0):
-        bot.send_message(user_id, "Please, confirm your order first.\n/confirm")
+        bot.send_message(user_id, "Пожалуйста, сначала подтвердите заказ.\n/confirm")
     else:
         confirm_code = randint(1000, 9999)
-        bot.send_message(user_id, "Your confirmation code is " + confirm_code.__str__()
+        bot.send_message(user_id, "Ваш код подтверждения " + confirm_code.__str__()
                      + "\nAverage delivery time is 60 min")
-
-"""    
-@bot.message_handler(content_types=['text'])
-def delete(message):
-    user_id = message.chat.id
-    num = ''
-    num = message.text
-    print(num)
-    print(num.isdigit())
-    if (num.isdigit() == True):
-        num = int(num)
-        if (num > 0 & num < 100):
-            #delete
-            bot.send_message(user_id, "number")
-        else:
-            bot.send_message(user_id, "Please, write propper order to delete.")
-#    else: above
-"""
     
 @bot.message_handler(func=lambda message: message.text.split('_')[-1] == 'cart')
 def add_to_cart(message):
@@ -211,7 +258,7 @@ def add_to_cart(message):
                 order[user_id] = thing + "\n"
             else:
                 order[user_id] += thing + "\n"
-            bot.send_message(user_id, thing + " is added to cart successfully.\n" + 'Type /cart to see your order.')
+            bot.send_message(user_id, thing + " успешно добавлено в корзину.\n" + 'Ваша корзина: /cart')
 
 @bot.message_handler(func=lambda message: message.text.split('_')[-1] == 'description')
 def description(message):
@@ -238,15 +285,15 @@ def description(message):
         end = start + len(thing)
         str = str[:start] + str[end:]
         order[user_id] = str
-        bot.send_message(user_id, "Delete successful")
+        bot.send_message(user_id, "Успешною")
         show_cart(message)
     else:
-        bot.send_message(user_id, "Sorry, you can't delete " + thing + "Check your /cart")
+        bot.send_message(user_id, f"Вы не можете удалить {thing}\nПроверьте корзину /cart")
 
 @bot.message_handler(content_types=['text'])
 def remember(message):
     user_id = message.chat.id
-    bot.send_message(user_id, "Hiii /help /start")
+    bot.send_message(user_id, "Приветик) /help")
 """
     item = allmenu.get(message.text, 0)
     if (item != 0):
